57eeb0825951f56dcd047e7c1845f19f5a3eb58d
ksm463 <113885176+ksm463@users.noreply.github.com>
2024-01-24 23:04:28 +0900

    [Bronze III] Title: 최댓값, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.py
+++ 백준/Bronze/2562. 최댓값/최댓값.py
@@ -1,6 +1,6 @@
-m = []
+a = []
 for i in range(9):
-    a = int(input())
-    m.append(a)
-print(max(m))
-print(m.index(max(m))+1)
+    b = int(input())
+    a.append(b)
+print(max(a))
+print(a.index(max(a))+1)
